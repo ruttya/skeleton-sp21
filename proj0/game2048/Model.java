2c467a7fcb1dbcbadefdeb53b9041c05327e551c
ruttya <arutty@163.com>
2024-12-19 15:53:28 +0800

    proj0

--- proj0/game2048/Model.java
+++ proj0/game2048/Model.java
@@ -5,7 +5,7 @@ import java.util.Observable;
 
 
 /** The state of a game of 2048.
- *  @author TODO: YOUR NAME HERE
+ *  @author ruttya
  */
 public class Model extends Observable {
     /** Current contents of the board. */
@@ -113,11 +113,18 @@ public class Model extends Observable {
         // TODO: Modify this.board (and perhaps this.score) to account
         // for the tilt to the Side SIDE. If the board changed, set the
         // changed local variable to true.
-
+        this.board.setViewingPerspective(side);
+        int s=board.size();
+        for(int j=0;j<s;j++){
+            for(int i=s-2;i>=0;i--){
+                ;
+            }
+        }
         checkGameOver();
         if (changed) {
             setChanged();
         }
+        this.board.setViewingPerspective(Side.NORTH);
         return changed;
     }
 
@@ -137,7 +144,13 @@ public class Model extends Observable {
      *  Empty spaces are stored as null.
      * */
     public static boolean emptySpaceExists(Board b) {
-        // TODO: Fill in this function.
+        // Fill in this function.
+        int s=b.size();
+        for( int i=0;i<s;i++){
+            for (int j=0;j<s;j++){
+                if (b.tile(i,j)==null)return true;
+            }
+        }
         return false;
     }
 
@@ -147,7 +160,13 @@ public class Model extends Observable {
      * given a Tile object t, we get its value with t.value().
      */
     public static boolean maxTileExists(Board b) {
-        // TODO: Fill in this function.
+        //  Fill in this function.
+        int s=b.size();
+        for( int i=0;i<s;i++){
+            for (int j=0;j<s;j++){
+                if (b.tile(i,j)!=null && b.tile(i,j).value()==MAX_PIECE)return true;
+            }
+        }
         return false;
     }
 
@@ -158,7 +177,16 @@ public class Model extends Observable {
      * 2. There are two adjacent tiles with the same value.
      */
     public static boolean atLeastOneMoveExists(Board b) {
-        // TODO: Fill in this function.
+        // Fill in this function.
+        if (emptySpaceExists(b)){return true;}
+        int s=b.size();
+        for( int i=0;i<s-1;i++){
+            for (int j=0;j<s-1;j++){
+                if (b.tile(i,j).value()==b.tile(i,j+1).value())return true;
+                if (b.tile(i,j).value()==b.tile(i+1,j).value())return true;
+            }
+            if (b.tile(i,s-1).value()==b.tile(i+1,s-1).value())return true;
+        }
         return false;
     }
 

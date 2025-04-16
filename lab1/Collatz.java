d93f2506836c17ac7694ffba41ff0e61d39d2be5
ruttya <arutty@163.com>
2025-04-16 22:09:36 +0800

    done with Collatz lab1commit

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -1,14 +1,18 @@
-/** Class that prints the Collatz sequence starting from a given number.
- *  @author YOUR NAME HERE
+/**
+ * Class that prints the Collatz sequence starting from a given number.
+ *
+ * @author ruttya
  */
 public class Collatz {
 
-    /** Buggy implementation of nextNumber! */
+    /**
+     * Buggy implementation of nextNumber!
+     */
     public static int nextNumber(int n) {
-        if (n%2  == 0) {
-            return n/2;
-        }else {
-            return n * 3+1;
+        if (n % 2 == 0) {
+            return n / 2;
+        } else {
+            return n * 3 + 1;
         }
     }
 

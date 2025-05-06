3bcc2961f1bb6e364ac8fa9f02e4cf0e95f87561
ruttya <arutty@163.com>
2025-05-06 16:09:27 +0800

    lab8 finish

--- lab8/hashmap/MyHashMap.java
+++ lab8/hashmap/MyHashMap.java
@@ -107,7 +107,7 @@ public class MyHashMap<K, V> implements Map61B<K, V> {
         return table;
     }
 
-    // TODO: Implement the methods of the Map61B Interface below
+    // Implement the methods of the Map61B Interface below
     // Your code won't compile until you do so!
     @Override
     public void clear() {
@@ -248,6 +248,4 @@ public class MyHashMap<K, V> implements Map61B<K, V> {
             return nodeIter.next().key;
         }
     }
-
-
 }

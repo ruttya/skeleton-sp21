3290784a9afe284f21364bbe460e5016cc22fe87
ruttya <arutty@163.com>
2025-05-21 14:46:36 +0800

    回退 gitlet/

--- proj2/gitlet/Main.java
+++ proj2/gitlet/Main.java
@@ -9,35 +9,16 @@ public class Main {
      *  <COMMAND> <OPERAND1> <OPERAND2> ... 
      */
     public static void main(String[] args) {
-        if (args.length == 0) {
-            System.out.println("Must have at least one argument");
-            System.exit(-1);
-        }
+        // TODO: what if args is empty?
         String firstArg = args[0];
         switch(firstArg) {
             case "init":
-                Repository.initial();
+                // TODO: handle the `init` command
                 break;
             case "add":
-                validateNumArgs("add", args, 2);
-                Repository.addFile(args[1]);
                 // TODO: handle the `add [filename]` command
                 break;
             // TODO: FILL THE REST IN
-        }
-    }
-    /**
-     * Checks the number of arguments versus the expected number,
-     * throws a RuntimeException if they do not match.
-     *
-     * @param cmd  Name of command you are validating
-     * @param args Argument array from command line
-     * @param n    Number of expected arguments
-     */
-    public static void validateNumArgs(String cmd, String[] args, int n) {
-        if (args.length != n) {
-            throw new RuntimeException(
-                    String.format("Invalid number of arguments for: %s.", cmd));
         }
     }
 }
--- proj2/gitlet/Repository.java
+++ proj2/gitlet/Repository.java
@@ -1,8 +1,6 @@
 package gitlet;
 
 import java.io.File;
-import java.io.IOException;
-
 import static gitlet.Utils.*;
 
 // TODO: any imports you need here
@@ -11,7 +9,7 @@ import static gitlet.Utils.*;
  *  TODO: It's a good idea to give a description here of what else this Class
  *  does at a high level.
  *
- *  @author ruttya
+ *  @author TODO
  */
 public class Repository {
     /**
@@ -21,7 +19,6 @@ public class Repository {
      * comment above them describing what that variable represents and how that
      * variable is used. We've provided two examples for you.
      */
-    private Commit[] commits;
 
     /** The current working directory. */
     public static final File CWD = new File(System.getProperty("user.dir"));
@@ -29,21 +26,4 @@ public class Repository {
     public static final File GITLET_DIR = join(CWD, ".gitlet");
 
     /* TODO: fill in the rest of this class. */
-    public static void initial(){
-        if (!GITLET_DIR.exists()){
-            GITLET_DIR.mkdir();
-        }else {
-            message("A Gitlet version-control system already exists in the current directory.");
-        }
-    }
-    //TODO: move file to somewhere in /.gilet
-    public static void addFile(String fileName){
-        File f=join(GITLET_DIR,fileName);
-        try {
-            f.createNewFile();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-
-    }
 }
